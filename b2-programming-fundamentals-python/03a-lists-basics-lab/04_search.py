49bb122fcd9b11c2cc1d239755d6d2076c85b9da
ivan-zgurov <163242655+ivan-zgurov@users.noreply.github.com>
2024-03-14 12:06:23 +0100

    Solution Added

---
+++ b2-programming-fundamentals-python/03a-lists-basics-lab/04_search.py
@@ -0,0 +1,14 @@
+number = int(input())
+word = input()
+strings = []
+
+for _ in range(number):
+    curent_string = input()
+    strings.append(curent_string)
+print(strings)
+
+for i in range(len(strings) - 1, -1, -1):
+    element = strings[i]
+    if word not in element:
+        strings.remove(element)
+print(strings)
