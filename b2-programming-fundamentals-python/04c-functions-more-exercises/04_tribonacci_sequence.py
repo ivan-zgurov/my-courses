c0af753abd0c8225c0e4a12dac9d2f3f50cf09c4
ivan-zgurov <163242655+ivan-zgurov@users.noreply.github.com>
2024-03-15 16:13:02 +0100

    Solution Added

---
+++ b2-programming-fundamentals-python/04c-functions-more-exercises/04_tribonacci_sequence.py
@@ -0,0 +1,15 @@
+def sequence(n):
+    nums = []
+    for _ in range(n):
+        if len(nums) == 2:
+            a = nums[-1] + nums[-2]
+        elif len(nums) > 2:
+            a = nums[-1] + nums[-2] + nums[-3]
+        else:
+            a = 1
+        nums.append(a)
+        print(a, end=" ")
+
+
+p = int(input())
+sequence(p)
