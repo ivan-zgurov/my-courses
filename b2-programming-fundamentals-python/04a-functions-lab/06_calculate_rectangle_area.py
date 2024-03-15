2cec9560a030a6f67327f5314d402cda5dc73474
ivan-zgurov <163242655+ivan-zgurov@users.noreply.github.com>
2024-03-15 16:06:09 +0100

    Solution Added

---
+++ b2-programming-fundamentals-python/04a-functions-lab/06_calculate_rectangle_area.py
@@ -0,0 +1,9 @@
+def calculate_area(width, height):
+    return width * height
+
+
+rect_width = int(input())
+rect_height = int(input())
+
+result = calculate_area(rect_width, rect_height)
+print(result)
