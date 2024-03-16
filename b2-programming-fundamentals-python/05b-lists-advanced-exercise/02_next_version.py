cd0cd445ba257d5d32b7acabd18cc0b109f0e2ce
ivan-zgurov <163242655+ivan-zgurov@users.noreply.github.com>
2024-03-16 19:41:16 +0100

    Solution Added

---
+++ b2-programming-fundamentals-python/05b-lists-advanced-exercise/02_next_version.py
@@ -0,0 +1,20 @@
+start_version = input()
+nums = start_version.split(".")
+
+value_found = False
+twice = False
+
+for i in range(1, 10):
+    if twice:
+        break
+    for j in range(10):
+        if twice:
+            break
+        for k in range(10):
+            if value_found:
+                twice = True
+                if [str(i), str(j), str(k)] != nums:
+                    print(f"{i}.{j}.{k}")
+                break
+            if [str(i), str(j), str(k)] == nums:
+                value_found = True
