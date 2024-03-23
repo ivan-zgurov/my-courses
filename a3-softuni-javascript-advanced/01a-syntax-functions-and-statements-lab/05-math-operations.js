103b9ada074e78cdc71f49a7c84be493a815e180
ivan-zgurov <163242655+ivan-zgurov@users.noreply.github.com>
2024-03-23 07:27:19 +0100

    Solution Added

---
+++ a3-softuni-javascript-advanced/01a-syntax-functions-and-statements-lab/05-math-operations.js
@@ -0,0 +1,22 @@
+function mathOperations(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      console.log(num1 + num2);
+      break;
+    case "-":
+      console.log(num1 - num2);
+      break;
+    case "*":
+      console.log(num1 * num2);
+      break;
+    case "/":
+      console.log(num1 / num2);
+      break;
+    case "%":
+      console.log(num1 % num2);
+      break;
+    default:
+      console.log(num1 ** num2);
+      break;
+  }
+}
