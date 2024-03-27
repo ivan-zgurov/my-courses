24f318d609db54d881bef32e053484e02bf3052c
ivan-zgurov <163242655+ivan-zgurov@users.noreply.github.com>
2024-03-27 09:00:22 +0100

    Solution Added

---
+++ a3-softuni-javascript-advanced/07b-unit-testing-and-error-handling-exercise/02-even-or-odd.js
@@ -0,0 +1,14 @@
+describe(`checks if length of passed string is odd or even`, () => {
+    it(`inout -> (a) -> odd`, () => {
+        expect(isOddOrEven('a')).to.eq('odd')
+    })
+    it(`input -> aa -> even`, () => {
+        expect(isOddOrEven('aa')).to.eq('even')
+    })
+    it(`input -> 1 -> undefined`, () => {
+        expect(isOddOrEven(1)).to.eq(undefined)
+    })
+    it(`input -> [a,a] -> undefined`, () => {
+        expect(isOddOrEven(['a', 'a'])).to.eq(undefined)
+    })
+})
