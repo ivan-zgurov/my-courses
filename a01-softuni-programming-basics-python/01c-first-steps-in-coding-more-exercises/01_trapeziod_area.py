7aa84f6af8f3da797587c7ae5301b54c7b6df991
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-08-16 09:37:30 +0200

    Solution Added

---
+++ a01-softuni-programming-basics-python/01c-first-steps-in-coding-more-exercises/01_trapeziod_area.py
@@ -0,0 +1,7 @@
+trapeziod_side_b1 = float(input())
+trapeziod_side_b2 = float(input())
+trapeziod_height_h = float(input())
+
+trapeziod_area = (trapeziod_side_b1 + trapeziod_side_b2) * trapeziod_height_h / 2
+
+print(f"{trapeziod_area:.2f}")
