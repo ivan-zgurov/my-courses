b9e68b8ef328ffa8275087076f58ea1f4a59bf76
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-08-16 08:57:03 +0200

    Solution Added

---
+++ a01-softuni-programming-basics-python/01a-first-steps-in-coding-lab/07_projects_creation.py
@@ -0,0 +1,8 @@
+architect_name = str(input())
+number_of_projects = int(input())
+
+hours_needed = 3 * number_of_projects
+
+print(
+    f"The architect {architect_name} will need {hours_needed} hours to complete {number_of_projects} project/s."
+)
