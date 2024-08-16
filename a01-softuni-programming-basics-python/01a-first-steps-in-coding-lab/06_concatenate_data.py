f2df4945180dbb5c87117afbc035186d4dde220e
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-08-16 08:54:28 +0200

    Solution Added

---
+++ a01-softuni-programming-basics-python/01a-first-steps-in-coding-lab/06_concatenate_data.py
@@ -0,0 +1,6 @@
+person_first_name = str(input())
+person_last_name = str(input())
+person_age = int(input())
+person_location = str(input())
+
+print(f"You are {person_first_name} {person_last_name}, a {person_age}-years old person from {person_location}.")
