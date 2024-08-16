420cc4b43ad272c53e3c590cc8052c7a87689686
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-08-16 10:19:15 +0200

    Solution Added

---
+++ a01-softuni-programming-basics-python/02a-conditional-statements-lab/07_area_of_figures.py
@@ -0,0 +1,26 @@
+from math import pi
+
+figure = input()
+area = 0
+
+if figure == "square":
+    side_a = float(input())
+    area = side_a**2
+    print(f"{area:.3f}")
+
+elif figure == "rectangle":
+    side_a = float(input())
+    side_b = float(input())
+    area = side_a * side_b
+    print(f"{area:.3f}")
+
+elif figure == "circle":
+    side_a = float(input())
+    area = side_a**2 * pi
+    print(f"{area:.3f}")
+
+elif figure == "triangle":
+    side_a = float(input())
+    h = float(input())
+    area = (side_a * h) / 2
+    print(f"{area:.3f}")
