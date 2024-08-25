35bf4a3cc8eca547c84e7cca1df8cc0ad4bf01ed
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-08-25 16:18:25 +0200

    Solution Added

---
+++ a01-softuni-programming-basics-python/04b-for-loop-exercise/02_half_sum_element.py
@@ -0,0 +1,19 @@
+count_of_numbers = int(input())
+sum_of_all_elements = 0
+max_number = " "
+
+for number in range(count_of_numbers):
+    current_number = int(input())
+    if number == 0:
+        max_number = current_number
+    sum_of_all_elements += current_number
+    if current_number > max_number:
+        max_number = current_number
+
+if max_number == sum_of_all_elements - max_number:
+    print("Yes")
+    print(f"Sum = {max_number}")
+else:
+    difference = abs(max_number - (sum_of_all_elements - max_number))
+    print("No")
+    print(f"Diff = {difference}")
