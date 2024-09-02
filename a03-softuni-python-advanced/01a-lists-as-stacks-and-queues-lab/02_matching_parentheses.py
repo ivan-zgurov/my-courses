6138da8d6c99f62b42123250dedf708b040db73f
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-09-02 09:21:27 +0300

    Solution Added

---
+++ a03-softuni-python-advanced/01a-lists-as-stacks-and-queues-lab/02_matching_parentheses.py
@@ -0,0 +1,10 @@
+algebraic_expression = input()
+stack = []
+
+for index in range(0, len(algebraic_expression)):
+    if algebraic_expression[index] == "(":
+        stack.append(index)
+    elif algebraic_expression[index] == ")":
+        start_index = stack.pop()
+        last_index = index + 1
+        print(algebraic_expression[start_index:last_index])
