02971849832bd8f6bd03668b75d07822b6e290fa
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-09-08 14:43:01 +0300

    Solution Added

---
+++ a03-softuni-python-advanced/02b-tuples-and-sets-exercise/02_sets_of_elements.py
@@ -0,0 +1,14 @@
+n, m = (int(x) for x in input().split())
+
+set_n = set()
+set_m = set()
+
+for _ in range(n):
+    set_n.add(input())
+
+for _ in range(m):
+    set_m.add(input())
+
+unique_elements = set_n.intersection(set_m)
+
+print("\n".join(unique_elements))
