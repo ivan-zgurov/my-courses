c934a1727f81e8b6b2500a9d8d55367fa0822313
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-09-08 14:43:10 +0300

    Solution Added

---
+++ a03-softuni-python-advanced/02b-tuples-and-sets-exercise/03_periodic_table.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+unique_elements = set()
+
+for _ in range(n):
+    elements = input().split()
+    for el in elements:
+        unique_elements.add(el)
+
+print(*unique_elements, sep="\n")
