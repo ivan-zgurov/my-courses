b79c692c8f9a580a93258b067ccba537198a3415
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-09-12 09:07:57 +0300

    Solution Added

---
+++ a03-softuni-python-advanced/03b-Multidimensional-lists-exercise-1/03_2x2_squares_in_matrix.py
@@ -0,0 +1,16 @@
+rows, columns = [int(x) for x in input().split()]
+matrix = [[x for x in input().split()] for _ in range(rows)]
+
+count = 0
+
+for row in range(rows - 1):
+    for col in range(columns - 1):
+        if (
+            matrix[row][col]
+            == matrix[row][col + 1]
+            == matrix[row + 1][col]
+            == matrix[row + 1][col + 1]
+        ):
+            count += 1
+
+print(count)
