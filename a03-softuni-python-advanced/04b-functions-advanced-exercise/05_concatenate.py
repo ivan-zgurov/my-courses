2279be1853ede6d9f32b735ff3c724f6b7d0b341
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-09-17 03:56:53 +0300

    Solution Added

---
+++ a03-softuni-python-advanced/04b-functions-advanced-exercise/05_concatenate.py
@@ -0,0 +1,10 @@
+def concatenate(*args, **kwargs):
+    result = ""
+    for string in args:
+        result += string
+
+    for key, value in kwargs.items():
+        if key in result:
+            result = result.replace(key, value)
+
+    return result
