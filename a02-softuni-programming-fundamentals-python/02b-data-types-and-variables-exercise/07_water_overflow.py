61be4370651e9fba7aa955c144e200e65f9abca5
ivan-zgurov <177826508+ivan-zgurov@users.noreply.github.com>
2024-11-23 09:51:31 +0200

    Solution Added

---
+++ a02-softuni-programming-fundamentals-python/02b-data-types-and-variables-exercise/07_water_overflow.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+capacity = 255
+liters_poured = 0
+
+for _ in range(n):
+    liters_add = int(input())
+    liters_poured += liters_add
+    if liters_poured > capacity:
+        liters_poured -= liters_add
+        print("Insufficient capacity!")
+
+print(liters_poured)
